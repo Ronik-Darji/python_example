80a9c764decf9f7b69de8442ed9589bdb09e1061
Ronik-Darji <ronikdarji037@gmail.com>
2023-03-14 16:23:55 +0530

    First Commit

---
+++ ledderif.py
@@ -0,0 +1,27 @@
+sname=input("enter student name :")
+rno=int(input("enter roll no :"))
+s1=int(input("enter mark of subject 1:"))
+s2=int(input("enter mark of subject 2:"))
+s3=int(input("enter mark of subject 3:"))
+
+
+total=s1+s2+s3
+per=total/3
+
+print("student name :",sname)
+print("roll no :",rno)
+print("total :",total)
+print("percentage :",per)
+
+if per>=70:
+         print("Distinction")
+elif per>=60:
+         print("first class")
+elif per>=50:
+         print("second class")
+elif per>=40:
+         print("pass class")
+
+else:
+         print("fail")
+    

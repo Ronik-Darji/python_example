162a09e886480afe7c4951e8ff490c93baabee9c
Ronik-Darji <ronikdarji037@gmail.com>
2023-03-30 12:38:47 +0530

    third commit

---
+++ String Demo.py
@@ -0,0 +1,36 @@
+s="Tops Technologies"
+
+print(s)
+print(s.capitalize())
+print(s.casefold())
+print(s.lower())
+print(s.upper())
+print(s.swapcase())
+print(s.center(40, "*"))
+print(s.count("T"))
+print(s.startswith("Tec"))
+print(s.endswith("ies"))
+print(s.find("h"))
+print(s.index("i"))
+print(s.title())
+
+
+print("Ronik001".isalpha())
+print("001".isalnum())
+print("Ronik".isalnum())
+
+
+print("Ronik".isalpha())
+print("001".isalpha())
+print("Ronik R".isalpha())
+
+
+print("Ronik001".isnumeric())
+print("001".isnumeric())
+print("001.08".isnumeric())
+
+
+print(" ".isspace())
+print(" R o n i k".isspace())
+
+print(s.replace("Tops Technologies","Python Program"))
---
+++ class student details.py
@@ -0,0 +1,19 @@
+class Student():
+
+    def getData(self,fname,lname):
+        self.fname = fname
+        self.lname = lname
+
+
+    def putData(self):
+        print("Student First Name :",self.fname)
+        print("Student Lirst Name :",self.lname)
+
+
+
+s1 = Student()
+fname = input("Enter First Name :")
+lname = input("Enter last Name :")
+
+s1.getData(fname,lname)
+s1.putData()
